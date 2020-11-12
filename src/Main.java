e14cf2d90da2408f6d0f5ab771cf53fb0a44957a
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-12 10:45:42 -0500

    added another comment

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello git last day");
         //added comment
+        System.out.println("Our head is moving with the new branch");
     }
 }
