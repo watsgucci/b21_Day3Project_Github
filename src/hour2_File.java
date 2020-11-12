2a2f5998535c281e880744d7f1f765eb2d07a66c
B21_Daniel_Vanshtein <vanshtein@gmail.com>
2020-11-12 11:20:57 -0500

    added a comment, laerned shortcut to commit (cntrl+K)

--- src/hour2_File.java
+++ src/hour2_File.java
@@ -1,5 +1,6 @@
 public class hour2_File {
     public static void main(String[] args) {
         System.out.println("We will be pushing a branch to remote");
+        //adding another comment
     }
 }
